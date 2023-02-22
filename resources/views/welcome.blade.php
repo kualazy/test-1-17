93c63a3543898accb17d3a612b9aeb19547cb823
Thanawat Polraksa <mrkuagamers@gmail.com>
2023-02-22 16:13:58 +0700

    1233

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>แนะนำตัว</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row mt-4 text-center">
+        <div class="col-7">
+            <h2>ธนวัฒน์  พลรักษา  เลขที่1  ห้อง ปวส.1/17</h2>
+        </div>
+        <div class="col">
+            <h2>{{date('D:M:Y H:i:s')}}</h2>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
